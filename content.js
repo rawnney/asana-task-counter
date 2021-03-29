4e586ef4be027a0cb1ecc3e54d609c4e372a1f00
Hugo Rune <hugo@bambuser.com>
2021-03-29 18:37:50 +0200

    awd

--- content.js
+++ content.js
@@ -1,29 +1,28 @@
-chrome.runtime.onMessage.addListener((msg, sender, /* sendResponse */ ) => {
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.text === "count") {
-    // sendResponse(countTasks());
-    countTasks()
+    sendResponse(countTasks());
   }
 });
 
 const countTasks = () => {
 
-  const columns = document.getElementsByClassName('BoardColumnScrollableContainer-cardsList')
-/*   const nrOfTasksArray = [] */
+  const columns = document.getElementsByClassName('BoardColumn');
+  const nrOfTasksArray = [];
 
   setTimeout(() => {
     for (let i = 0; i < columns.length; i++) {
-      const column = columns[i]
-      let headerTextElement = document.getElementsByClassName('BoardColumnHeader')[i].firstElementChild
-      let count = column.children.length - 1
-      if (count === 15) count = '15+'
-      text = headerTextElement.innerText + ` (${count})`
-      headerTextElement.innerText = text
+      const column = columns[i];
+      const items = column.getElementsByClassName('SortableItem');
+      let headerTextElement = document.getElementsByClassName('BoardColumnHeader')[i].firstElementChild;
+      let count = items.length;
+      if (count === 15) count = '15+';
+      text = headerTextElement.innerText + ` (${count})`;
+      headerTextElement.innerText = text;
 
-  /*     nrOfTasksArray.push(count) */
+      nrOfTasksArray.push(count);
     }
-  }, 2000)
+  }, 2000);
 
- // return nrOfTasksArray
- return null;
+  return nrOfTasksArray;
 };
 
